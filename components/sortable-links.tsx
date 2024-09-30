c7fea0f17f2d10649bf6e432420a1cadb80e99c6
John Joseph <johnjosephnc@gmail.com>
2024-09-30 11:12:31 -0400

    Add entry tooltips

--- components/sortable-links.tsx
+++ components/sortable-links.tsx
@@ -20,6 +20,12 @@ import {
   FileMinusIcon
 } from "@radix-ui/react-icons"
 import { EntryData } from "./page/entries-editor"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip"
 
 interface Item {
   id: number
@@ -75,70 +81,99 @@ const SortableLinks: FC<SortableLinkCardProps> = ({
 
   const isCursorGrabbing = attributes["aria-pressed"]
 
-  console.log("draft", draft)
-
   return (
     <div ref={setNodeRef} style={style} key={uniqueId}>
-      <Card
-        className={`relative flex justify-between gap-5 group  ${editable ? "border-slate-400" : null}`}
-      >
-        <div className="min-w-0">{children}</div>
-        <div className="flex justify-center items-center gap-4 min-w-10 pr-2">
-          <div className="hidden group-hover:block">
-            <Dialog>
-              <DialogTrigger>
-                <Cross1Icon className="text-destructive" />
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>
-                    Are you sure you want to delete this entry?
-                  </DialogTitle>
-                  <DialogFooter>
-                    <DialogClose asChild>
-                      <Button variant="destructive" onClick={handleDelete}>
-                        Delete
-                      </Button>
-                    </DialogClose>
-                  </DialogFooter>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
+      <TooltipProvider>
+        <Card
+          className={`relative flex justify-between gap-5 group  ${editable ? "border-slate-400" : null}`}
+        >
+          <div className="min-w-0">{children}</div>
+          <div className="flex justify-center items-center gap-4 min-w-10 pr-2">
+            <div className="hidden group-hover:block">
+              <Dialog>
+                <Tooltip>
+                  <DialogTrigger>
+                    <TooltipTrigger asChild>
+                      <Cross1Icon className="text-destructive" />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Delete entry</p>
+                    </TooltipContent>
+                  </DialogTrigger>
+                </Tooltip>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>
+                      Are you sure you want to delete this entry?
+                    </DialogTitle>
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="destructive" onClick={handleDelete}>
+                          Delete
+                        </Button>
+                      </DialogClose>
+                    </DialogFooter>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <div className="hidden group-hover:block cursor-pointer">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <CheckIcon className="text-save" onClick={handleClick} />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Save entry</p>
+                </TooltipContent>
+              </Tooltip>
+            </div>
+            <div className="hidden group-hover:block cursor-pointer">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Pencil1Icon className="text-save" onClick={handleEdit} />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Edit</p>
+                </TooltipContent>
+              </Tooltip>
+            </div>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                {draft ? (
+                  <FilePlusIcon
+                    className="hidden group-hover:block cursor-pointer"
+                    onClick={handleDraftToggle}
+                  />
+                ) : (
+                  <FileMinusIcon
+                    className="hidden group-hover:block cursor-pointer"
+                    onClick={handleDraftToggle}
+                  />
+                )}
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{draft ? "Publish entry" : "Unpublish entry"}</p>
+              </TooltipContent>
+            </Tooltip>
+
+            {sortable ? (
+              <button
+                {...attributes}
+                {...listeners}
+                className={` ${isCursorGrabbing ? "cursor-grabbing" : "cursor-grab"} hidden group-hover:block`}
+                aria-describedby={`DndContext-${uniqueId}`}
+              >
+                <svg viewBox="0 0 20 20" width="15">
+                  <path
+                    d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </button>
+            ) : null}
           </div>
-          <div className="hidden group-hover:block cursor-pointer">
-            <CheckIcon className="text-save" onClick={handleClick} />
-          </div>
-          <div className="hidden group-hover:block cursor-pointer">
-            <Pencil1Icon className="text-save" onClick={handleEdit} />
-          </div>
-          {draft ? (
-            <FilePlusIcon
-              className="hidden group-hover:block"
-              onClick={handleDraftToggle}
-            />
-          ) : (
-            <FileMinusIcon
-              className="hidden group-hover:block"
-              onClick={handleDraftToggle}
-            />
-          )}
-          {sortable ? (
-            <button
-              {...attributes}
-              {...listeners}
-              className={` ${isCursorGrabbing ? "cursor-grabbing" : "cursor-grab"} hidden group-hover:block`}
-              aria-describedby={`DndContext-${uniqueId}`}
-            >
-              <svg viewBox="0 0 20 20" width="15">
-                <path
-                  d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </button>
-          ) : null}
-        </div>
-      </Card>
+        </Card>
+      </TooltipProvider>
     </div>
   )
 }
