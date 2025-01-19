dcf3c70daac1170bb52d80d5ce5bec52bf5d5eb9
johnjoseph3 <j.joseph@avalo.ai>
2025-01-19 14:11:14 -0500

    Condense entry actions

--- components/sortable-links.tsx
+++ components/sortable-links.tsx
@@ -17,7 +17,8 @@ import {
   CheckIcon,
   Pencil1Icon,
   FilePlusIcon,
-  FileMinusIcon
+  FileMinusIcon,
+  DotsHorizontalIcon
 } from "@radix-ui/react-icons"
 import { EntryData } from "./page/entries-editor"
 import {
@@ -26,6 +27,15 @@ import {
   TooltipProvider,
   TooltipTrigger
 } from "@/components/ui/tooltip"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu"
 
 interface Item {
   id: number
@@ -83,99 +93,122 @@ const SortableLinks: FC<SortableLinkCardProps> = ({
 
   return (
     <div ref={setNodeRef} style={style} key={uniqueId}>
-      <TooltipProvider>
-        <Card
-          className={`relative flex justify-between gap-5 group  ${editable ? "border-slate-400" : null}`}
-        >
-          <div className="min-w-0" style={{ maxWidth: "75%" }}>
-            {children}
-          </div>
-          <div className="flex justify-center items-center gap-4 min-w-10 pr-2">
-            <div className="hidden group-hover:block">
-              <Dialog>
-                <Tooltip>
-                  <DialogTrigger>
-                    <TooltipTrigger asChild>
-                      <Cross1Icon className="text-destructive" />
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <p>Delete entry</p>
-                    </TooltipContent>
-                  </DialogTrigger>
-                </Tooltip>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>
-                      Are you sure you want to delete this entry?
-                    </DialogTitle>
-                    <DialogFooter>
-                      <DialogClose asChild>
-                        <Button variant="destructive" onClick={handleDelete}>
-                          Delete
-                        </Button>
-                      </DialogClose>
-                    </DialogFooter>
-                  </DialogHeader>
-                </DialogContent>
-              </Dialog>
-            </div>
-            <div className="hidden group-hover:block cursor-pointer">
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <CheckIcon className="text-save" onClick={handleClick} />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Save entry</p>
-                </TooltipContent>
-              </Tooltip>
-            </div>
-            <div className="hidden group-hover:block cursor-pointer">
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Pencil1Icon className="text-save" onClick={handleEdit} />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Edit</p>
-                </TooltipContent>
-              </Tooltip>
-            </div>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                {draft ? (
-                  <FilePlusIcon
-                    className="hidden group-hover:block cursor-pointer"
-                    onClick={handleDraftToggle}
-                  />
-                ) : (
-                  <FileMinusIcon
-                    className="hidden group-hover:block cursor-pointer"
-                    onClick={handleDraftToggle}
-                  />
-                )}
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{draft ? "Publish entry" : "Unpublish entry"}</p>
-              </TooltipContent>
-            </Tooltip>
+      <Card
+        className={`relative flex justify-between gap-5 group  ${editable ? "border-slate-400" : null}`}
+      >
+        <div className="min-w-0" style={{ maxWidth: "75%" }}>
+          {children}
+        </div>
 
-            {sortable ? (
-              <button
-                {...attributes}
-                {...listeners}
-                className={` ${isCursorGrabbing ? "cursor-grabbing" : "cursor-grab"} hidden group-hover:block`}
-                aria-describedby={`DndContext-${uniqueId}`}
-              >
-                <svg viewBox="0 0 20 20" width="15">
-                  <path
-                    d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-              </button>
-            ) : null}
+        <div className="flex justify-center items-center gap-4 min-w-10 pr-2 opacity-30 hover:opacity-100">
+          {sortable ? (
+            <button
+              {...attributes}
+              {...listeners}
+              className={` ${isCursorGrabbing ? "cursor-grabbing" : "cursor-grab"}`}
+              aria-describedby={`DndContext-${uniqueId}`}
+            >
+              <svg viewBox="0 0 20 20" width="15">
+                <path
+                  d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </button>
+          ) : null}
+
+          <div className="cursor-pointer relative ">
+            <TooltipProvider>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <DotsHorizontalIcon />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" forceMount className="min-w-0">
+                  <DropdownMenuItem className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <Dialog>
+                        <Tooltip>
+                          <DialogTrigger>
+                            <TooltipTrigger asChild>
+                              <Cross1Icon className="text-destructive" />
+                            </TooltipTrigger>
+                            <TooltipContent side="left">
+                              <p>Delete</p>
+                            </TooltipContent>
+                          </DialogTrigger>
+                        </Tooltip>
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle>
+                              Are you sure you want to delete this entry?
+                            </DialogTitle>
+                            <DialogFooter>
+                              <DialogClose asChild>
+                                <Button
+                                  variant="destructive"
+                                  onClick={handleDelete}
+                                >
+                                  Delete
+                                </Button>
+                              </DialogClose>
+                            </DialogFooter>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
+                    </div>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Tooltip>
+                      <TooltipTrigger
+                        asChild
+                        className="text-save cursor-pointer"
+                      >
+                        <CheckIcon onClick={handleClick} />
+                      </TooltipTrigger>
+                      <TooltipContent side="left">
+                        <p>Save</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Tooltip>
+                      <TooltipTrigger
+                        asChild
+                        className="text-save cursor-pointer"
+                      >
+                        <Pencil1Icon onClick={handleEdit} />
+                      </TooltipTrigger>
+                      <TooltipContent side="left">
+                        <p>Edit</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        {draft ? (
+                          <FilePlusIcon
+                            className="group-hover:block cursor-pointer"
+                            onClick={handleDraftToggle}
+                          />
+                        ) : (
+                          <FileMinusIcon
+                            className="group-hover:block cursor-pointer"
+                            onClick={handleDraftToggle}
+                          />
+                        )}
+                      </TooltipTrigger>
+                      <TooltipContent side="left">
+                        <p>{draft ? "Publish" : "Unpublish"}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TooltipProvider>
           </div>
-        </Card>
-      </TooltipProvider>
+        </div>
+      </Card>
     </div>
   )
 }
