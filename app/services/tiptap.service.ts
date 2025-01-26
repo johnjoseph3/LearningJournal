e1e523a6b7326c8e2763072df243d188e9943921
johnjoseph3 <j.joseph@avalo.ai>
2025-01-26 07:39:42 -0500

    Refactor delete images

--- app/services/tiptap.service.ts
+++ app/services/tiptap.service.ts
@@ -1,13 +1,4 @@
-import { PrismaClient } from "@prisma/client"
-import { awsService } from "./aws.service"
-
 class TipTapService {
-  private prisma: PrismaClient
-
-  constructor() {
-    this.prisma = new PrismaClient()
-  }
-
   extractImageUrls(json: any): string[] {
     const imageUrls: string[] = []
     if (json === null || json === undefined) {
@@ -25,34 +16,6 @@ class TipTapService {
 
     traverse(json)
     return imageUrls
-  }
-
-  async deleteImages(model: string, id: number) {
-    let imageUrls: string[] = []
-
-    switch (model) {
-      case "Entry":
-        const entry = await this.prisma.entry.findUnique({ where: { id } })
-        if (!entry) throw new Error("Entry not found")
-        imageUrls = this.extractImageUrls(entry?.content)
-        break
-      case "Topic":
-        const pages = await this.prisma.page.findMany({
-          where: { topicId: id }
-        })
-        const entries = await this.prisma.entry.findMany({
-          where: { pageId: { in: pages.map((page) => page.id) } }
-        })
-        for (const entry of entries) {
-          imageUrls = imageUrls.concat(this.extractImageUrls(entry.content))
-        }
-        break
-      default:
-        break
-    }
-    for (const url of imageUrls) {
-      await awsService.deleteS3Image(url)
-    }
   }
 }
 
--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 import { tipTapService } from "@/app/services/tiptap.service"
+import { awsService } from "@/app/services/aws.service"
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
@@ -9,11 +10,39 @@ const prisma =
     query: {
       $allModels: {
         async delete({ model, operation, args, query }) {
-          if (model === "Entry" || model === "Topic") {
+          let imageUrls: string[] = []
+
+          // Delete images from S3 when deleting an entry or a topic
+          if (model === "Entry") {
             const id = args.where.id
             if (!id) throw new Error("ID is required to delete")
-            await tipTapService.deleteImages(model, id)
+
+            const entry = await prisma.entry.findUnique({ where: { id } })
+            if (!entry) throw new Error("Entry not found")
+
+            imageUrls = tipTapService.extractImageUrls(entry?.content)
+          } else if (model === "Topic") {
+            const id = args.where.id
+            if (!id) throw new Error("ID is required to delete")
+
+            const pages = await prisma.page.findMany({
+              where: { topicId: id }
+            })
+            const entries = await prisma.entry.findMany({
+              where: { pageId: { in: pages.map((page) => page.id) } }
+            })
+
+            for (const entry of entries) {
+              imageUrls = imageUrls.concat(
+                tipTapService.extractImageUrls(entry.content)
+              )
+            }
           }
+
+          for (const url of imageUrls) {
+            await awsService.deleteS3Image(url)
+          }
+
           return query(args)
         }
       }
